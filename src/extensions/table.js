42faf8b386fdb05ce06bad7802047ab602fd9d44
zwirn1981 <zwirn1981@users.noreply.github.com>
2014-02-16 19:06:14 +0100

    Update table.js
    fixing bug: tbl = [];

--- src/extensions/table.js
+++ src/extensions/table.js
@@ -81,6 +81,8 @@
             tbl.push('</table>');
             // we are done with this table and we move along
             out.push(tbl.join('\n'));
+            //reset tbl to allow multiple tables
+            tbl = [];
             continue;
           }
         }
